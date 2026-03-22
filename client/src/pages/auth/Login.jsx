732d6ece983b1e10de7fbfd65de499163953ae83
YongR4I <raihandaffa19072009@gmail.com>
2026-03-22 22:22:24 +0700

    bikin fitur landing page contoh

--- client/src/pages/auth/Login.jsx
+++ client/src/pages/auth/Login.jsx
@@ -1,4 +1,6 @@
-import { Link } from 'react-router-dom'
+import { Link
+
+ } from 'react-router-dom'
 
 export default function Login() {
   return (
@@ -13,7 +15,7 @@ export default function Login() {
       </form>
       <div className="mt-3 flex flex-col gap-2 text-sm text-white/80">
         <Link className="hover:text-white" to="/password-recovery">Forgot password?</Link>
-        <span>No account? <Link className="text-white font-semibold" to="/register">Create an account</Link></span>
+       
       </div>
     </div>
   )
