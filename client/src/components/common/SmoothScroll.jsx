5a926a24efa97e1786f589801a601074d65a4705
YongR4I <raihandaffa19072009@gmail.com>
2026-03-26 03:59:57 +0700

    add smooth scroll

---
+++ client/src/components/common/SmoothScroll.jsx
@@ -0,0 +1,29 @@
+import { useEffect } from 'react';
+import Lenis from 'lenis';
+import 'lenis/dist/lenis.css';
+
+const SmoothScroll = () => {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      smoothWheel: true,
+      wheelMultiplier: 1.1,
+    });
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
+  return null; // Komponen ini hanya menjalankan side effect
+};
+
+export default SmoothScroll;
