03610f3d89c916cbe76983499ef6f9681091e820
YongR4I <raihandaffa19072009@gmail.com>
2026-03-26 00:45:51 +0700

    smooth scrolling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,34 @@
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Lenis from 'lenis'
+import 'lenis/dist/lenis.css'
 import HomePage from './pages/HomePage';
 import ClassPage from './pages/ClassPage';
 import BlogPage from './pages/BlogPage';
 import AIAssistPage from './pages/AIAssistPage';
 
 function App() {
+  useEffect(() => {
+    // Initialize Lenis
+    const lenis = new Lenis({
+      duration: 1.5,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      smoothWheel: true,
+      wheelMultiplier: 1.1,
+    });
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
   return (
     <Router>
       <Routes>
