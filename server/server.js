53c635ab4825b7f7b927a9c0a29d82dbaa5c71f2
YongR4I <raihandaffa19072009@gmail.com>
2026-04-12 13:54:30 +0700

    fix: resolve merge conflicts

--- server/server.js
+++ server/server.js
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
-import app from './src/app.js'
-
-const port = process.env.PORT || 5000
-=======
 require('dotenv').config({ path: '../.env' });
 const app = require('./src/app');
 const port = process.env.PORT || 5000;
->>>>>>> auth-backend
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console
